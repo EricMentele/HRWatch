e13370e2878260080172dc91656594800b238eec
Eric Mentele <ericmentele@gmail.com>
2024-02-20 22:18:15 -0600

    Add initial Xcode project setup for tests

---
+++ HRWatch/HRWatch/HRWatch.h
@@ -0,0 +1,18 @@
+//
+//  HRWatch.h
+//  HRWatch
+//
+//  Created by Eric Mentele on 2/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HRWatch.
+FOUNDATION_EXPORT double HRWatchVersionNumber;
+
+//! Project version string for HRWatch.
+FOUNDATION_EXPORT const unsigned char HRWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HRWatch/PublicHeader.h>
+
+
